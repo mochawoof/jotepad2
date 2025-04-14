3e10f9e8d5625b2776697b50efbacfcdf597dcc2
Julian <julianherbert.2007@gmail.com>
2025-04-14 08:28:32 -0400

    rewrite

--- src/DetailsFileChooser.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.event.*;
-
-class DetailsFileChooser extends JFileChooser {
-    public DetailsFileChooser() {
-        Action detailsAction = getActionMap().get("viewTypeDetails");
-        if (detailsAction != null) {detailsAction.actionPerformed(null);}
-    }
-}
